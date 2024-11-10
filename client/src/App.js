860bde585510082ea93cbfb394d6e7ac6b4074f7
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2024-11-09 17:47:35 -0800

    Finished auth0 sign in/sign out

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
 
 function App() {
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
   const [message, setMessage] = useState('');
 
   useEffect(() => {
@@ -14,6 +17,15 @@ function App() {
     <div>
       <h1>Message from Backend:</h1>
       <p>{message}</p>
+
+      {!isAuthenticated ? (
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+      ) : (
+        <>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>
+          <h2>Welcome, {user.name}</h2>
+        </>
+      )}
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = "dev-z1p4dq5h1lfacble.us.auth0.com";
+const clientId = "Cd6amcblJE1eBOQ1L8WGuAEVngZlpMTK";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
