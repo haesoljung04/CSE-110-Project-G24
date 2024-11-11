8f92159671b8fc5cfcf8412ea1e810fb6cdc5d59
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2024-11-10 23:30:50 -0800

    deleted default react test that was causing test failure

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
 
 // sanity check
 test('renders login button when not authenticated', () => {
