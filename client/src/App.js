605f6561bb7cf206838740a80a25de5a922bcb69
noemyvalencia <130100414+noemyvalencia@users.noreply.github.com>
2024-11-15 12:42:20 -0800

    AddFriend Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AddFriend from './pages/AddFriend';
+
 
 function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('/api')
+    fetch('http://localhost:5001/api')
       .then((response) => response.json())
       .then((data) => setMessage(data.message))
       .catch((error) => console.error('Error:', error));
   }, []);
 
   return (
-    <div>
-      <h1>Message from Backend:</h1>
-      <p>{message}</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/add-friend" element={<AddFriend />} />
+        {/* Other routes */}
+      </Routes>
+    </Router>
   );
 }
 
