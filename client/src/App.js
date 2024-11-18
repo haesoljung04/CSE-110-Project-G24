82b865d9d80ce271e42ba04665bb00548a1df3ed
SamH314 <samuelhernandez010904@gmail.com>
2024-11-17 19:09:50 -0800

    almost finsih w/ feature/settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { Settings } from './pages/Settings';
 
 function App() {
   const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
@@ -26,17 +27,7 @@ function App() {
   
   return (
     <div>
-      <h1>Message from Backend:</h1>
-      <p>{message}</p>
-
-      {!isAuthenticated ? (
-        <button onClick={() => loginWithRedirect()}>Log In</button>
-      ) : (
-        <>
-          <button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>
-          <h2>Welcome, {user.name}</h2>
-        </>
-      )}
+      <Settings />
     </div>
   );
 }
---
+++ client/src/pages/Settings.js
@@ -0,0 +1,64 @@
+import React, {useState} from "react";
+import { useAuth0 } from '@auth0/auth0-react';
+import '../styles/Settings.css'
+
+export const Settings = () => {
+    const { logout } = useAuth0();
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+    const handleDeleteClick = () => {
+        setShowDeleteModal(true);
+    };
+
+    const handleCancelDelete = () => {
+        setShowDeleteModal(false);
+    };
+
+    const handleConfirmDelete = () => {
+        //neeed to finish this!!!!
+        console.log("Account deleted");
+        setShowDeleteModal(false);
+    };
+
+    return (
+        <div className="settings-container">
+            <h1 className="settings-title">Settings</h1>
+            
+            <div className="center-buttons">
+            <button className="edit-profile-image">Edit</button>
+            <button className="edit-profile">Edit Profile</button>
+            </div>
+
+            <div className="ghost-mode">
+    <label>Ghost mode</label>
+    <label className="slider">
+        <input type="checkbox" />
+        <span className="slider-container"></span>
+    </label>
+</div>
+
+<div className="light-dark-mode">
+    <label>Light/Dark mode</label>
+    <label className="slider">
+        <input type="checkbox" />
+        <span className="slider-container"></span>
+    </label>
+</div>
+
+            <p onClick={() => logout({ returnTo: window.location.origin })}>Log Out</p>
+
+            <p className="reset-password">Reset Password</p>
+            <p className="delete-account" onClick={handleDeleteClick}>Delete Account</p>
+
+            {showDeleteModal && (
+                <div className="modal-overlay">
+                    <div className="modal-content">
+                        <p>Are you sure?</p>
+                        <button onClick={handleConfirmDelete} className="modal-delete-button">Delete</button>
+                        <button onClick={handleCancelDelete} className="modal-cancel-button">Keep Account</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
