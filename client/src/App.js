38ce47bfb172101fef75a06d5875067804e16f9c
devvbella <ava0712@naver.com>
2024-11-17 16:33:20 -0800

    friendAll frontend done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import FriendsList from './pages/FriendsList'; 
 
 function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('/api')
+    fetch('http://localhost:5001/api')
       .then((response) => response.json())
       .then((data) => setMessage(data.message))
       .catch((error) => console.error('Error:', error));
