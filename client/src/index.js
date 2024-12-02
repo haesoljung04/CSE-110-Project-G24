24cb686fdbdfa752c52ea42726294a344b430d8b
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2024-12-02 15:03:06 -0800

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styles/dark-mode.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { ThemeProvider } from './context/ThemeContext';
 
+const savedTheme = localStorage.getItem('darkMode') === 'true';
+if (savedTheme) {
+  document.body.classList.add('dark-mode');
+}
 const domain = "dev-z1p4dq5h1lfacble.us.auth0.com";
 const clientId = "Cd6amcblJE1eBOQ1L8WGuAEVngZlpMTK";
 
@@ -16,7 +22,9 @@ root.render(
       clientId={clientId}
       redirectUri={window.location.origin}
     >
+      <ThemeProvider>
       <App />
+      </ThemeProvider>
     </Auth0Provider>
   </React.StrictMode>
 );
